b98d69c4dd097d37f660a79882347f9a37119b26
ArtiFixal <89784139+ArtiFixal@users.noreply.github.com>
2025-05-04 21:44:12 +0200

    init

---
+++ BikeManagementSystemLib/Models/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeManagementSystem.Models
+{
+    public abstract class BaseEntity<T>
+    {
+        [Key]
+        public T Id { get; set; }
+
+        public BaseEntity(T id)
+        {
+            Id = id;
+        }
+    }
+}
