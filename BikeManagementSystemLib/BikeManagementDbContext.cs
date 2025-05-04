85afbe63f41e156646cee9e873cd786ab67e0b3c
ArtiFixal <89784139+ArtiFixal@users.noreply.github.com>
2025-05-04 23:13:09 +0200

    Create DB context

---
+++ BikeManagementSystemLib/BikeManagementDbContext.cs
@@ -0,0 +1,44 @@
+﻿using BikeManagementSystem.Models;
+using BikeManagementSystemLib.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeManagementSystem
+{
+    public class BikeManagementDbContext : DbContext
+    {
+        public BikeManagementDbContext(DbContextOptions options) : base(options){}
+
+        public DbSet<Bike> Bikes { get; set; }
+        public DbSet<BikeType> BikeTypes { get; set; }
+        public DbSet<Vendor> Vendors { get; set; }
+        public DbSet<Image> Images { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseLazyLoadingProxies();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Bike>(entity =>
+            {
+                entity.HasOne(bike => bike.Type)
+                .WithMany(type => type.Bikes)
+                .HasForeignKey(bike => bike.TypeId);
+
+                entity.HasOne(bike => bike.Vendor)
+                .WithMany(vendor => vendor.Bikes)
+                .HasForeignKey(bike => bike.VendorId);
+
+                entity.HasOne(bike => bike.Image)
+                .WithMany(image => image.Bikes)
+                .HasForeignKey(bike => bike.ImageId);
+            });
+        }
+    }
+}
