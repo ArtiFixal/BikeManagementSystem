6aef6f3d291b1735033231e3ec98dec8cc120014
ArtiFixal <89784139+ArtiFixal@users.noreply.github.com>
2025-05-05 21:26:05 +0200

    Add VendorService

---
+++ BikeManagementSystemLib/Services/VendorService.cs
@@ -0,0 +1,12 @@
+﻿using BikeManagementSystemLib.Models;
+
+namespace BikeManagementSystemLib.Services
+{
+    /// <summary>
+    /// Service related to <see cref="BikeManagementSystemLib.Models.Bike">>Bike</see> vendors.
+    /// </summary>
+    public class VendorService : CrudService<long, Vendor>
+    {
+        public VendorService(BikeManagementDbContext context) : base(context,context.Vendors){}
+    }
+}
