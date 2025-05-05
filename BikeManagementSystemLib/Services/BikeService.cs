41dc8ad8a1fdc0272d038415dde9427afcb94b23
ArtiFixal <89784139+ArtiFixal@users.noreply.github.com>
2025-05-05 22:03:27 +0200

    Add BikeService

---
+++ BikeManagementSystemLib/Services/BikeService.cs
@@ -0,0 +1,33 @@
+﻿using BikeManagementSystemLib.Exceptions;
+using BikeManagementSystemLib.Models;
+
+namespace BikeManagementSystemLib.Services
+{
+    /// <summary>
+    /// Service related to Bikes.
+    /// </summary>
+    public class BikeService: CrudService<long,Bike>
+    {
+        public BikeService(BikeManagementDbContext context): base(context,context.Bikes){}
+
+        /// <summary>
+        /// Sends given bike to maintenace.
+        /// </summary>
+        /// 
+        /// <param name="bikeId">Which bike to send</param>
+        /// <exception cref="EntityNotFoundException">If bike was not found</exception>
+        public Maintenance MaintenanceBike(int bikeId,string maintenanceDescription)
+        {
+            Maintenance maintenance=new Maintenance(bikeId,DateTime.Now,maintenanceDescription);
+            GetEntity(bikeId).LastMaintenance = maintenance;
+            context.SaveChanges();
+            return maintenance;
+        }
+
+        public List<Bike> GetBikesReqiringMaintenance()
+        {
+            return entitySet.Where(bike => bike.Durability < 40)
+                .ToList();
+        }
+    }
+}
