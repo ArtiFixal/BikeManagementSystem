5b804d922b1f915a333feca334c2ba46317ba374
ArtiFixal <89784139+ArtiFixal@users.noreply.github.com>
2025-05-05 21:26:31 +0200

    Add BikeTypeService

---
+++ BikeManagementSystemLib/Services/BikeTypeService.cs
@@ -0,0 +1,12 @@
+﻿using BikeManagementSystemLib.Models;
+
+namespace BikeManagementSystemLib.Services
+{
+    /// <summary>
+    /// Service related to <see cref="Bike">Bike</see> types.
+    /// </summary>
+    public class BikeTypeService : CrudService<int, BikeType>
+    {
+        public BikeTypeService(BikeManagementDbContext context) : base(context, context.BikeTypes){}
+    }
+}
