7651351fa302552747c120bcb4a9404a2d93284c
ArtiFixal <89784139+ArtiFixal@users.noreply.github.com>
2025-05-05 21:01:26 +0200

    Add common DB operations

--- BikeManagementSystemLib/Services/CrudService.cs
+++ BikeManagementSystemLib/Services/CrudService.cs
@@ -1,5 +1,7 @@
 ﻿using BikeManagementSystem;
 using BikeManagementSystem.Models;
+using BikeManagementSystemLib.Exceptions;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,11 +18,24 @@ namespace BikeManagementSystemLib.Services
     /// <typeparam name="ET">Entity type</typeparam>
     public abstract class CrudService<ID,ET> where ET : BaseEntity<ID>
     {
+        public static readonly int DEFAULT_PAGE_SIZE = 20;
         protected BikeManagementDbContext context;
 
-        protected CrudService(BikeManagementDbContext context)
+        /// <summary>
+        /// Entity Dbset from context.
+        /// </summary>
+        protected DbSet<ET> entitySet;
+
+        /// <summary>
+        /// Entity short name.
+        /// </summary>
+        private readonly String entityName;
+
+        protected CrudService(BikeManagementDbContext context, DbSet<ET> entitySet)
         {
             this.context = context;
+            this.entitySet = entitySet;
+            entityName=typeof(ET).Name;
         }
 
         /// <summary>
@@ -29,7 +44,43 @@ namespace BikeManagementSystemLib.Services
         /// 
         /// <exception cref="BikeManagementSystemLib.Exceptions.EntityNotFoundException">If entity was not found</exception>
         /// <returns>Entity from DB</returns>
-        public abstract ET GetEntity(ID entityID);
+        public virtual ET GetEntity(ID entityID)
+        {
+            return entitySet.FirstOrDefault(entity => entity.Id.Equals(entityID))
+                ?? throw new EntityNotFoundException(entityName,entityID);
+        }
+
+        public long CountEntities()
+        {
+            return entitySet.Count();
+        }
+
+        public int GetPageCount(int pageSize)
+        {
+            return (int)(CountEntities() / pageSize);
+        }
+
+        /// <summary>
+        /// Retreives page of entities from DB.
+        /// </summary>
+        /// 
+        /// <param name="entitySet">From where to retreive data</param>
+        /// <param name="pageNumber">Which page</param>
+        /// <param name="pageSize">How many elements to get</param>
+        /// <returns>Page of entities</returns>
+        public virtual List<ET> GetEntityPage(int pageNumber,int pageSize)
+        {
+            int toSkip = (pageNumber - 1) * pageSize;
+            return entitySet.Skip(toSkip)
+                .Take(pageSize)
+                .ToList();
+        }
+
+        public virtual List<ET> GetAllEntities()
+        {
+            return entitySet.ToList();
+        }
+
         public virtual ID AddEntity(ET entity)
         {
             return context.Add(entity).Entity.Id;
