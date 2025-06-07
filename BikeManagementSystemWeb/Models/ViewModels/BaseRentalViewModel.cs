ef2ca66e58ae2615da96d613be634b971b2b30a6
ArtiFixal <89784139+ArtiFixal@users.noreply.github.com>
2025-06-07 19:31:47 +0200

    Add BaseRentalViewModel

---
+++ BikeManagementSystemWeb/Models/ViewModels/BaseRentalViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BikeManagementSystemWeb.Models.ViewModels
+{
+    public abstract class BaseRentalViewModel: BaseViewModel<long?>
+    {
+        public ClientViewModel Client { get; set; }
+        public DateTime RentedFrom { get; set; }
+        public DateTime RentedTo { get; set; }
+    }
+}
